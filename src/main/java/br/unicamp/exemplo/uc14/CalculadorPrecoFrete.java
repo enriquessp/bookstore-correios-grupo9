7282294f89f4f9e9d215a7d87f1d51c03e99fea8
enrique.pereira <enrique.pereira@reservafacil.tur.br>
2016-10-04 23:49:46 -0300

    Inicia implementacao do wiremock

--- src/main/java/br/unicamp/exemplo/uc14/CalculadorPrecoFrete.java
+++ src/main/java/br/unicamp/exemplo/uc14/CalculadorPrecoFrete.java
@@ -1,14 +1,41 @@
 package br.unicamp.exemplo.uc14;
 
 import br.unicamp.dominio.Produto;
+import com.raps.code.generate.ws.CResultado;
+import com.raps.code.generate.ws.CalcPrecoPrazoWS;
+import com.raps.code.generate.ws.CalcPrecoPrazoWSSoap;
+
+import java.math.BigDecimal;
 
 /**
  * Created by enrique1 on 10/4/16.
  */
 public class CalculadorPrecoFrete {
 
+    private final CalcPrecoPrazoWSSoap servicoCorreriosWS;
+
+    public CalculadorPrecoFrete(CalcPrecoPrazoWSSoap servicoCorreriosWS) {
+        this.servicoCorreriosWS = servicoCorreriosWS;
+    }
+
     public double calcularPreco(Produto produto, String cep) {
-        return 50;
+        CResultado resultadoConsultaCorreio = servicoCorreriosWS.calcPrecoPrazo("empresa",
+                "", //senha
+                "", //servico
+                cep,
+                cep,
+                "" + produto.getPeso(),
+                0,//formato
+                BigDecimal.valueOf(produto.getComprimento()),
+                BigDecimal.valueOf(produto.getAltura()),
+                BigDecimal.valueOf(produto.getLargura()),
+                BigDecimal.ZERO, //diametro
+                "", //maoPropria
+                BigDecimal.ZERO, //valorDeclarado
+                "" //avisoRecebimento
+        );
+
+        return Double.valueOf(resultadoConsultaCorreio.getServicos().getCServico().get(0).getValor());
     }
 
 }
--- src/test/java/br/unicamp/exemplo/steps/uc14/CalculadorPrecoFreteSteps.java
+++ src/test/java/br/unicamp/exemplo/steps/uc14/CalculadorPrecoFreteSteps.java
@@ -2,16 +2,29 @@ package br.unicamp.exemplo.steps.uc14;
 
 
 import br.unicamp.dominio.Produto;
-import br.unicamp.exemplo.Calculadora;
 import br.unicamp.exemplo.uc14.CalculadorPrecoFrete;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.http.RequestListener;
+import com.github.tomakehurst.wiremock.http.Response;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.raps.code.generate.ws.CalcPrecoPrazoWS;
+import com.raps.code.generate.ws.CalcPrecoPrazoWSSoap;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+
+import javax.xml.ws.BindingProvider;
+
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertNotNull;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 public class CalculadorPrecoFreteSteps {
 
@@ -26,10 +39,87 @@ public class CalculadorPrecoFreteSteps {
     private double precoFrete;
     private Throwable throwable;
 
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule();
+
     @Before
     public void setUp() {
-    	calculadora = new CalculadorPrecoFrete();
+        wireMockRule.stubFor(get(urlEqualTo("/some/thing"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/xml")
+                        .withBody("<Servicos>" +
+                                "<cServico>" +
+                                "<Codigo>40010</Codigo>" +
+                                "<Valor>17,20</Valor>" +
+                                "<PrazoEntrega>1</PrazoEntrega>" +
+                                "<ValorSemAdicionais>17,20</ValorSemAdicionais>" +
+                                "<ValorMaoPropria>0,00</ValorMaoPropria>" +
+                                "<ValorAvisoRecebimento>0,00</ValorAvisoRecebimento>" +
+                                "<ValorValorDeclarado>0,00</ValorValorDeclarado>" +
+                                "<EntregaDomiciliar>S</EntregaDomiciliar>" +
+                                "<EntregaSabado>S</EntregaSabado>" +
+                                "<Erro>0</Erro>" +
+                                "<MsgErro/>" +
+                                "</cServico>" +
+                                "</Servicos>")));
+
+        wireMockRule.start();
+        if (wireMockRule.isRunning()) {
+            System.out.print("No ar");
+        }
+
+        CalcPrecoPrazoWSSoap servicoDosCorreiosWS = new CalcPrecoPrazoWS().getCalcPrecoPrazoWSSoap();
+        BindingProvider bp = (BindingProvider)servicoDosCorreiosWS;
+        bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:8080/some/thing");
+
+        calculadora = new CalculadorPrecoFrete(servicoDosCorreiosWS);
     	throwable = null;
+    }
+
+    @After
+    public void close() {
+        wireMockRule.shutdown();
+    }
+
+    @org.junit.Before
+    public void prepare() {
+        wireMockRule.stubFor(get(urlEqualTo("/CalcPrecoPrazo"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/xml")
+//                        .withHeader("Content-Type", "text/html")
+                        .withBody("<Servicos>" +
+                                "<cServico>" +
+                                "<Codigo>40010</Codigo>" +
+                                "<Valor>17,20</Valor>" +
+                                "<PrazoEntrega>1</PrazoEntrega>" +
+                                "<ValorSemAdicionais>17,20</ValorSemAdicionais>" +
+                                "<ValorMaoPropria>0,00</ValorMaoPropria>" +
+                                "<ValorAvisoRecebimento>0,00</ValorAvisoRecebimento>" +
+                                "<ValorValorDeclarado>0,00</ValorValorDeclarado>" +
+                                "<EntregaDomiciliar>S</EntregaDomiciliar>" +
+                                "<EntregaSabado>S</EntregaSabado>" +
+                                "<Erro>0</Erro>" +
+                                "<MsgErro/>" +
+                                "</cServico>" +
+                                "</Servicos>")));
+
+        wireMockRule.start();
+
+        if (wireMockRule.isRunning()) {
+            System.out.print("No ar");
+        }
+
+        CalcPrecoPrazoWSSoap servicoDosCorreiosWS = new CalcPrecoPrazoWS().getCalcPrecoPrazoWSSoap();
+        BindingProvider bp = (BindingProvider)servicoDosCorreiosWS;
+        bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:8080");
+
+        calculadora = new CalculadorPrecoFrete(servicoDosCorreiosWS);
+        throwable = null;
+    }
+
+    @Test
+    public void exactUrlOnly() {
+        calculadora.calcularPreco(new Produto(1, 2, 2, 4), "0120231");
     }
 
     @Given("^Dado um produto valido com peso (\\d+) largura (\\d+) altura (\\d+) comprimento (\\d+) e cep (\\d+)$")
